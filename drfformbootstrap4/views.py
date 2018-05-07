e62989f8f3cce66169b30b85229214b08f5deb78
Matthew Fox <matt@tansen.ca>
2018-05-07 07:23:41 -0700

    Remove unused import

--- drfformbootstrap4/views.py
+++ drfformbootstrap4/views.py
@@ -5,7 +5,6 @@ from .serializers import DemoSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer
-from django.shortcuts import redirect
 
 
 class DemoForms(APIView):
